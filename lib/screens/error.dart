b162d6f4b9a71d5f1e33a18614ffd779d24f8815
Dimas <dimasekaputra2000@gmail.com>
2025-06-01 09:39:54 +0700

    fix: overflow on error screen

--- lib/screens/error.dart
+++ lib/screens/error.dart
@@ -11,35 +11,38 @@ class ErrorScreen extends StatelessWidget {
       body: SafeArea(
         bottom: false,
         child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Image.asset('images/404.png', width: 300),
-              const SizedBox(height: 70),
-              Text('Cari apa hayoo?', style: AppFonts.dark.copyWith(fontSize: 24)),
-              const SizedBox(height: 14),
-              Text(
-                'Fitur yang kamu cari saat ini belum tersedia. \nTunggu update berikutnya ya!',
-                style: AppFonts.secondary.copyWith(fontSize: 16),
-                textAlign: TextAlign.center,
-              ),
-              const SizedBox(height: 50),
-              Container(
-                width: 210,
-                height: 50,
-                child: ElevatedButton(
-                  style: const ButtonStyle(
-                    backgroundColor: WidgetStatePropertyAll(AppColors.primary)
-                  ),
-                  onPressed: () {
-                    Navigator.push(context, MaterialPageRoute(builder: (context) => const HomeScreen()));
-                  },
-                  child: Text(
-                    'Kembali',
-                    style: AppFonts.light.copyWith(fontSize: 14),
-                  )),
-              )
-            ],
+          child: SingleChildScrollView(
+            padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 32),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Image.asset('images/404.png', width: 300),
+                const SizedBox(height: 70),
+                Text('Cari apa hayoo?', style: AppFonts.dark.copyWith(fontSize: 24)),
+                const SizedBox(height: 14),
+                Text(
+                  'Fitur yang kamu cari saat ini belum tersedia. \nTunggu update berikutnya ya!',
+                  style: AppFonts.secondary.copyWith(fontSize: 16),
+                  textAlign: TextAlign.center,
+                ),
+                const SizedBox(height: 50),
+                Container(
+                  width: 210,
+                  height: 50,
+                  child: ElevatedButton(
+                    style: const ButtonStyle(
+                      backgroundColor: WidgetStatePropertyAll(AppColors.primary)
+                    ),
+                    onPressed: () {
+                      Navigator.push(context, MaterialPageRoute(builder: (context) => const HomeScreen()));
+                    },
+                    child: Text(
+                      'Kembali',
+                      style: AppFonts.light.copyWith(fontSize: 14),
+                    )),
+                )
+              ],
+            ),
           ),
         ),
       ),
